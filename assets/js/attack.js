b8fe2b8b3af6ef869c9e80d5bed441f5091d3642
kuanyulll <guan9468@gmail.com>
2022-06-20 11:36:21 +0800

    0620-完成

--- assets/js/attack.js
+++ assets/js/attack.js
@@ -1,19 +1,83 @@
-$('.popup-btn').click(function () {
-  var href = $(this).attr('href');
-  $(href).fadeIn(250);
-  $(href)
-    .children$('popup-box')
-    .removeClass('transform-out')
-    .addClass('transform-in');
-  e.preventDefault();
+// 旅遊地圖按鈕1
+$(function () {
+  var $modal = $('.modal');
+  var HIDE_CLASS = 'is-hide';
+
+  $('#js-startbtn').on('click', function () {
+    $modal.removeClass(HIDE_CLASS);
+  });
+
+  $('.js-modal-close').on('click', function () {
+    $modal.addClass(HIDE_CLASS);
+  });
 });
 
-$('.popup-close').click(function () {
-  closeWindow();
+// 旅遊地圖按鈕2
+$(function () {
+  var $modal = $('.modal2');
+  var HIDE_CLASS = 'is2-hide';
+
+  $('#js2-startbtn').on('click', function () {
+    $modal.removeClass(HIDE_CLASS);
+  });
+
+  $('.js-modal2-close').on('click', function () {
+    $modal.addClass(HIDE_CLASS);
+  });
 });
 
-function closeWindow() {
-  $('.popup-wrap').fadeOut(200);
-  $('.popup-box').removeClass('transform-in').addClass('transform-out');
-  event.preventDefault();
-}
+// 旅遊地圖按鈕3
+$(function () {
+  var $modal = $('.modal3');
+  var HIDE_CLASS = 'is3-hide';
+
+  $('#js3-startbtn').on('click', function () {
+    $modal.removeClass(HIDE_CLASS);
+  });
+
+  $('.js-modal3-close').on('click', function () {
+    $modal.addClass(HIDE_CLASS);
+  });
+});
+
+// 旅遊地圖按鈕4
+$(function () {
+  var $modal = $('.modal4');
+  var HIDE_CLASS = 'is4-hide';
+
+  $('#js4-startbtn').on('click', function () {
+    $modal.removeClass(HIDE_CLASS);
+  });
+
+  $('.js-modal4-close').on('click', function () {
+    $modal.addClass(HIDE_CLASS);
+  });
+});
+
+// 旅遊地圖按鈕5
+$(function () {
+  var $modal = $('.modal5');
+  var HIDE_CLASS = 'is5-hide';
+
+  $('#js5-startbtn').on('click', function () {
+    $modal.removeClass(HIDE_CLASS);
+  });
+
+  $('.js-modal5-close').on('click', function () {
+    $modal.addClass(HIDE_CLASS);
+  });
+});
+
+// 旅遊地圖按鈕6
+$(function () {
+  var $modal = $('.modal6');
+  var HIDE_CLASS = 'is6-hide';
+
+  $('#js6-startbtn').on('click', function () {
+    $modal.removeClass(HIDE_CLASS);
+  });
+
+  $('.js-modal6-close').on('click', function () {
+    $modal.addClass(HIDE_CLASS);
+  });
+});
